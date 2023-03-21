0aa9cc5c497ec60a481fa3e3c37626e32990b31a
MATHEUS HENRIQUE DO AMARAL PAULINO <theush933@gmail.com>
2023-03-21 16:52:07 -0300

    first commit

---
+++ src/exercicio/pratico/Calculadora.java
@@ -0,0 +1,21 @@
+package exercicio.pratico;
+
+public class Calculadora {
+    public static void soma (double numero1,double numero2){
+        double resultado = numero1+numero2;
+        System.out.println("A soma dos dois numero e: "+resultado);
+
+    }
+    public static void subtracao (double numero1,double numero2){
+        double resultado = numero1-numero2;
+        System.out.println("A subtracao dos dois numero e: "+resultado);
+    }
+    public static void multiplicacao (double numero1,double numero2){
+        double resultado = numero1*numero2;
+        System.out.println("A multiplicacao dos dois numero e: "+resultado);
+    }
+    public static void divisao (double numero1,double numero2){
+        double resultado = numero1/numero2;
+        System.out.println("A divisao dos dois numero e: "+resultado);
+    }
+}
---
+++ src/exercicio/pratico/Main.java
@@ -0,0 +1,22 @@
+package exercicio.pratico;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercicio calculadora com os numeros 6, 3: ");
+        Calculadora.soma(6,3);
+        Calculadora.subtracao(6,3);
+        Calculadora.multiplicacao(6,3);
+        Calculadora.divisao(6,3);
+
+        System.out.println("Exercicio mensagem com as horas 9, 14, 20: ");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(20);
+
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+    }
+}
